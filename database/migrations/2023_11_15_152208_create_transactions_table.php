62f5bbd9d502b39b07cbb43afc577b8792e58589
Junn Eric Timoteo <juntyms@yahoo.com>
2023-11-15 21:26:48 +0400

    creation of migration transactions table

--- database/migrations/2023_11_15_152208_create_transactions_table.php
+++ database/migrations/2023_11_15_152208_create_transactions_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
+            $table->decimal('amount',8,3);
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+            $table->timestamp('due_on');
+            $table->decimal('vat',2,2);
+            $table->boolean('is_vat')->default(true);
             $table->timestamps();
         });
     }
