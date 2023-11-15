649b9f8c44550a8be737027ec55b778eb91bf20e
Junn Eric Timoteo <juntyms@yahoo.com>
2023-11-15 20:55:53 +0400

    register and login of user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,10 +2,52 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Traits\HttpResponses;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\LoginUserRequest;
+use App\Http\Requests\StoreUserRequest;
 
 class AuthController extends Controller
 {
     use HttpResponses;
+
+    public function register(StoreUserRequest $request)
+    {
+        $request->validated($request->all());
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return $this->success([
+            'user' => $user,
+            'token' => $user->createToken('API Token of '. $user->name)->plainTextToken
+        ]);
+    }
+
+    public function login(LoginUserRequest $request)
+    {
+        $request->validated($request->all());
+
+        $user = User::where('email',$request->email)->first();
+
+        if (!$user || !Auth::attempt($request->only('email','password'))) {
+            return $this->error('','Credentials do not match',401);
+        }
+
+        return $this->success([
+            'user' => $user,
+            'token' => $user->createToken('Api Token of '. $user->name)->plainTextToken
+        ]);
+    }
+
+    public function logout()
+    {
+        return response()->json('This is logout.');
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TransactionsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('v1')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login',[AuthController::class, 'login']);
+    Route::post('/logout',[AuthController::class, 'logout']);
+
+    Route::resource('/transactions', TransactionsController::class);
+});
+
